b40ba9c1b33ea5a68328bfcf0cb3643b3a1531ec
Tsutomu-Sato-0424 <tsutomu.satou@gmail.com>
2017-02-17 23:06:56 +0000

    modified controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, toy app"
   end
 
 #bensk8:~/workspace/toy_app (master) $ heroku create
 #Creating app... done, ⬢ vast-everglades-34116
-#https://vast-everglades-34116.herokuapp.com/ | https://git.heroku.com/vast-everglades-34116.git
+#https://sheltered-garden-13218.herokuapp.com/
 
 
 end
