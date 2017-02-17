37d36b9cb01041e45a3a4540426ccbcdddd74b8b
Tsutomu-Sato-0424 <tsutomu.satou@gmail.com>
2017-02-17 22:46:48 +0000

    Update gemfile.lock for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+
+#bensk8:~/workspace/toy_app (master) $ heroku create
+#Creating app... done, ⬢ vast-everglades-34116
+#https://vast-everglades-34116.herokuapp.com/ | https://git.heroku.com/vast-everglades-34116.git
+
+
 end
